d6259958095ea42e247d4660ca639b6e91c2be74
phuonganh <pphuonganh815@gmail.com>
2026-03-08 16:10:45 +0700

    project

--- utils/axiosInstance.ts
+++ utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://192.168.0.106:8084/api",
+    baseURL: "http://192.168.1.224:8084/api",
     headers: {
         "Content-Type": "application/json",
     },
